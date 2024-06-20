c539f4111770e0d8ab23f18a73fea108d647a2a4
france0707 <francesalvador.fos@gmail.com>
2024-06-20 13:37:11 +0800

    update

---
+++ tdl-app/app/ui/AddButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddButton = () => {
+	return (
+		<button className="bg-green-400 px-4 rounded-lg text-center">
+			ADD
+		</button>
+	);
+};
+
+export default AddButton;
--- tdl-app/app/ui/AddTask.tsx
+++ tdl-app/app/ui/AddTask.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AddButton from "./AddButton";
 
 const AddTask = () => {
 	return (
@@ -7,9 +8,7 @@ const AddTask = () => {
 				type="text"
 				className="w-3/4 px-2 py-1 border-2 rounded-lg"
 			></input>
-			<button className="bg-green-400 px-4 rounded-lg text-center">
-				ADD
-			</button>
+			<AddButton></AddButton>
 		</div>
 	);
 };
