f2a63209d5279601a80ab0ca29d9cd35e9bb4379
france0707 <francesalvador.fos@gmail.com>
2024-07-01 14:18:05 +0800

    update
    colors

--- tdl-app/app/ui/PendingButton.tsx
+++ tdl-app/app/ui/PendingButton.tsx
@@ -11,7 +11,7 @@ const PendingButton = () => {
 		<button
 			className={`w-auto h-auto px-2 py-1 font-medium text-center rounded-md transform transition-transform duration-100 focus:outline-none active:scale-90 ${
 				isPending
-					? "bg-slate-400 hover:bg-slate-500"
+					? "bg-amber-200 hover:bg-amber-400"
 					: "bg-green-400 hover:bg-green-600"
 			}`}
 			onClick={handleClick}
